db804da762a670bd89d4a5da5ad460bd93b18aa7
frailjohnarmamento6-tech <joryalmacin@gmail.com>
2025-12-03 01:06:19 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,3 +1,12 @@
 <?php
-// DB config placeholder
+$host = "localhost";
+$db_user = "root";
+$db_pass = "";
+$db_name = "bsit_portal";
+
+$conn = new mysqli($host, $db_user, $db_pass, $db_name);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 ?>
