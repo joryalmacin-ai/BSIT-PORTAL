8f0c242e329b2e46b8b0f563c3946f5c6398447b
frailjohnarmamento6-tech <joryalmacin@gmail.com>
2025-12-03 01:07:28 +0800

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,1 +1,4 @@
-<h1>BSIT Portal Home</h1>
+<?php include 'inc/header.php'; ?>
+<h1>Welcome to BSIT Portal</h1>
+<p>This is the homepage. Navigate through the menu to explore announcements, events, faculty, and more.</p>
+<?php include 'inc/footer.php'; ?>
